1c1e182b185212ab66ca6aeed22a0d69e0a76b30
Steve Fernandez <stevedominicfez@gmail.com>
2021-09-30 06:20:59 +0530

    precheck final

--- assignment4.cpp
+++ assignment4.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+class badch{};
+class invalid{};
 class employee
-{
-private:
+{ 
+public:
     int empno{};
     std::string name{};
     float salary{}, allowance{};
-
-public:
     employee()
     {
         empno = 0;
@@ -16,19 +16,34 @@ public:
         salary = 0;
         allowance = 0;
     }
-    void input(std::string n, int num)
+    void input()
     {
-        name = n;
-        empno = num;
+        std::cout<<"Enter Id and Name\n";
+        std::cin>>empno;
+        if(empno<0)
+            throw invalid();
+        std::getline(std::cin>>std::ws,name);
+        for (int i = 0; i < name.length(); i++)
+            if (std::isdigit(name[i]) == 0)
+                throw invalid();
         salary = 30000;
         allowance = 10000;
     }
-    void input(float s, std::string n, float a, int num)
+    void input(int n)
     {
-        empno = num;
-        name = n;
-        salary = s;
-        allowance = a;
+        if(n==2)
+        {
+            std::cout << "Enter Id, Name, Salary and Allowance\n";
+            std::cin >> empno;
+            if (empno < 0)
+                throw invalid();
+            std::getline(std::cin >> std::ws, name);
+            for(int i=0;i<name.length();i++)
+                if (std::isdigit(name[i]) == 0)
+                    throw invalid();
+            std::cin>>salary;
+            std::cin>>allowance;
+        }
     }
     employee operator++(int)
     {
@@ -59,31 +74,31 @@ public:
         std::cout << "\n";
     }
     ~employee() {}
-}
+};
 class manager:public employee
 {
-    int empno{};
-    std::string name{};
-    float salary{}, allowance{};
     public:
+        manager()
+        {
+            empno = 0;
+            name = "NULL";
+            salary = 0;
+            allowance = 0;
+        }
         manager operator++(int)
         {
             manager e = *this;
-            salary += 5000;
-            allowance += 500;
+            salary += 50000;
+            allowance += 5000;
             return e;
         }
-
-        //Operator overloading for the purpose of deducting the salary of an employee
         manager operator--(int)
         {
             manager e = *this;
-            salary -= 1000;
-            allowance -= 500;
+            salary -= 5000;
+            allowance -= 2000;
             return e;
         }
-
-        //Operator overloading to copy values from one employee object to another
         manager operator=(manager e)
         {
             empno = e.empno;
@@ -92,4 +107,56 @@ class manager:public employee
             allowance = e.allowance;
             return *this;
         }
+};
+void details(int n,employee *e,manager *m)
+{
+    if(n==1)
+    {
+        std::cout<<"\nEmployee\n";
+        e->input();
+        e->disp();
+        std::cout <<"\nManager\n";
+        m->input();
+        m->disp();
+    }
+    else if(n==2)
+    {
+        std::cout << "\nEmployee\n";
+        e->input(n);
+        e->disp();
+        std::cout << "\nManager\n";
+        m->input(n);
+        m->disp();
+    }
+}
+int main()
+{
+    int ch;
+    employee e;
+    manager m;
+    try
+    {
+        std::cout<<"1.New Recruits\n2.Existing\nChoice: ";
+        std::cin>>ch;
+        if(ch!=1 && ch!=2)
+            throw badch();
+        details(ch,&e,&m);
+        e.disp();
+        m.disp();
+        std::cout<<"\nAwarding Bonuses\n";
+        e++;
+        m++;
+        std::cout<<"\nEmployee";
+        e.disp();
+        std::cout<<"\nManager";
+        m.disp();
+    }
+    catch(badch) 
+    {
+        std::cout<<"\nINVALID ENTRY OF CHOICE(1|2 ONLY)"; 
+    }
+    catch(invalid) 
+    {
+        std::cout<<"\nIMPROPER DATA ENTRY";
+    }
 }
